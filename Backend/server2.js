7c85c744488d65e9f38cad4e9fed3e3d9e484c27
Ali Correa <aliagustin98@gmail.com>
2024-08-29 18:06:11 -0300

    Configuracion de BD ignorada para que no sea publica y server2 creado funcionando con conexion a bd externa

---
+++ Backend/server2.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { connectToDatabase } = require('./src/config/bdHelper');
+//const { createMember, getMembers, updateMember, deleteMember } = require('./members');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Conectar a la base de datos
+connectToDatabase();
+/*
+// Rutas de la API
+app.post('/miembros', async (req, res) => {
+  const { nombre, email } = req.body;
+  await createMember(nombre, email);
+  res.send('Miembro creado');
+});
+
+app.get('/miembros', async (req, res) => {
+  const miembros = await getMembers();
+  res.json(miembros);
+});
+
+app.put('/miembros/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nombre, email } = req.body;
+  await updateMember(id, nombre, email);
+  res.send('Miembro actualizado');
+});
+
+app.delete('/miembros/:id', async (req, res) => {
+  const { id } = req.params;
+  await deleteMember(id);
+  res.send('Miembro eliminado');
+});
+*/
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo!');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
---
+++ Backend/src/config/bdHelper.js
@@ -0,0 +1,26 @@
+const sql = require('mssql');
+
+// Configuración de la base de datos
+const config = {
+  user: 'principal', 
+  password: 'Analistas1', 
+  server: 'nodo1.database.windows.net', 
+  database: 'Principal', 
+  options: {
+    encrypt: true, // necesario para Azure
+    trustServerCertificate: false,
+    enableArithAbort: true
+  }
+};
+
+// Conexión a la base de datos
+async function connectToDatabase() {
+  try {
+    await sql.connect(config);
+    console.log('Conectado a la base de datos.');
+  } catch (err) {
+    console.error('Error de conexión con la base de datos:', err);
+  }
+}
+
+module.exports = { connectToDatabase, sql };
