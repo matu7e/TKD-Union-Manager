f9e6bfc60e959ff4e758022a8ba268420d85fa6b
mati <86624@sistemas.frc.utn.edu.ar>
2024-09-01 16:47:18 -0300

    Termine Front public

--- Frontend/controllers/menu.js
+++ Frontend/controllers/menu.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const hamburger = document.getElementById("hamburger");
-    const navbarMenu = document.getElementById("navbar-menu");
-  
-    hamburger.addEventListener("click", function() {
+  const hamburger = document.getElementById("hamburger");
+  const navbarMenu = document.getElementById("navbar-menu");
+  const menuLinks = navbarMenu.querySelectorAll("a"); // Selecciona todos los enlaces en el menú
+
+  hamburger.addEventListener("click", function() {
       navbarMenu.classList.toggle("open");
       document.body.classList.toggle("no-scroll");
-    });
   });
+
+  menuLinks.forEach(link => {
+      link.addEventListener("click", function() {
+          navbarMenu.classList.remove("open");
+          document.body.classList.remove("no-scroll");
+      });
+  });
+});
--- LEER.md
+++ LEER.md
@@ -12,3 +12,26 @@ crear dentro de TKD-Union-Manager un archivo llamado ".gitignore" con el siguien
     - backend/node_modules/
     - backend/uploads/
     - backend/src/config/
+
+
+
+
+
+# fond
+.libre-baskerville-regular {
+  font-family: "Libre Baskerville", serif;
+  font-weight: 400;
+  font-style: normal;
+}
+
+.libre-baskerville-bold {
+  font-family: "Libre Baskerville", serif;
+  font-weight: 700;
+  font-style: normal;
+}
+
+.libre-baskerville-regular-italic {
+  font-family: "Libre Baskerville", serif;
+  font-weight: 400;
+  font-style: italic;
+}
